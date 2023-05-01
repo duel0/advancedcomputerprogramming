0fed5d3bd2b5a8b9909a4f05a2e6aaee9e3e5045
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2023-05-01 11:10:37 +0200

    Primi Esempi Rifatti

---
+++ Francesco/04-JavaScript/Example1/app.js
@@ -0,0 +1,7 @@
+var main = function(){
+    "use strict"
+
+    window.alert("Hello, World!");
+}
+
+$(document).ready(main);
---
+++ Francesco/04-JavaScript/Example2/app.js
@@ -0,0 +1,25 @@
+var main = function() {
+    "use strict"
+
+    var addCommentFromInputBox = function(){
+        var $new_comment;
+        if($(".comment-input input").val()!==""){
+            $new_comment=$("<p>").text($(".comment-input input").val());
+            $new_comment.hide();
+            $(".comments").append($new_comment);
+            $new_comment.fadeIn();
+            $(".comment-input input").val("");
+        }
+    };
+
+    $(".comment-input button").on("click", function(event){
+        addCommentFromInputBox();
+    });
+    $(".comment-input input").on("keypress", function(event){
+        if(event.key==="Enter"){
+            addCommentFromInputBox();
+        }
+    });
+}
+
+$().ready(main);
